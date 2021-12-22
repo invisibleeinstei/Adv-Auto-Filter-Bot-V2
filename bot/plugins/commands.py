c9bfa8e3719027360ee41f36cd23e26c67e80b22
invisibleeinstei <91374376+invisibleeinstei@users.noreply.github.com>
2021-12-22 21:27:51 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/apealkuppiya"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/apealkuppiya'),
+        InlineKeyboardButton('Our group', url ='https://t.me/apealkuppiya')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Our Channel', url='https://t.me/alevelkuppiya1')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
